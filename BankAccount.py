18fb4d35ec248f40af178b239c30e7f8b733b212
sree-chikati <srechi453.fusdk12@gmail.com>
2020-11-04 23:18:49 -0800

    Step 3: A bank account should have attributes

---
+++ BankAccount.py
@@ -0,0 +1,7 @@
+class BankAccount:
+    #Attributes:
+    def __init__(self, full_name, account_number, routing_number, balance):
+        self.full_name = full_name
+        self.account_number = account_number
+        self.routing_number = routing_number
+        self.balance = 0
