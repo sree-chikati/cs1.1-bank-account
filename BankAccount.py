189f8b61af283d5780cb0c1c401572fd858e9fd7
sree-chikati <srechi453.fusdk12@gmail.com>
2020-11-09 22:37:45 -0800

    Step 5: Examples using Bank Account

--- BankAccount.py
+++ BankAccount.py
@@ -1,4 +1,4 @@
-from random
+from random import randint
 class BankAccount:
     #Step 3: Attributes:
     def __init__(self, full_name, account_number, routing_number, balance):
@@ -17,6 +17,7 @@ class BankAccount:
 
         self.balance += amount
         print(f"Amount Deposited: ${amount}")
+        print("\n")
 
     #Witdraw Method
     def withdraw(self, amount):
@@ -34,6 +35,7 @@ class BankAccount:
         else:
             self.balance -= amount
             print(f"Amount Withdrawn: ${amount}")
+        print("\n")
     
     #Get_Balance Method
     def get_balance(self):
@@ -41,6 +43,7 @@ class BankAccount:
         balance and then also return the current balance of the account. """
         print(f"Your current balance is: ${self.balance}")
         return self.balance
+        print("\n")
 
     #Add_interest Method
     def add_interest(self):
@@ -50,6 +53,7 @@ class BankAccount:
         following equation: interest = balance *  0.00083 """
         interest = balance *  0.00083
         self.balance += round(interest, 2)
+        print("\n")
 
     #Print_receipt Method
     def print_receipt(self):
@@ -59,13 +63,47 @@ class BankAccount:
         Routing No.: 98765432
         Balance: $100.00   
         """
-        account_no = str(self.account_number)
+        account_num = str(self.account_number)
 
         print(f"Account Holder: {self.full_name}")
-        print(f"Account number: ****{account_no[-4:]}")
+        print(f"Account number: ****{account_num[-4:]}")
         print(f"Routing number: {self.routing_number}")
         print(f"Balance: ${self.balance}")
+        print("\n")
     
+
+#Step 5: Outside of the BankAccount class, define 3 different bank account examples using the BankAccount() object.
+# Your examples should show you using the different methods above to demonstrate them working.
+
+def createAccountNo():
+    """8 digit account number generator"""
+    account_no = ""
+    for i in range(8):
+        account_no += str(randint(0, 9))
+    return int(account_no)
+
+# Account 1
+Sree = BankAccount("Aldrin", createAccountNo(), 123456789, 0)
+Sree.balance = 10
+Sree.withdraw(5)
+Sree.deposit(10)
+Sree.print_receipt()
+
+# Account 2
+Hiro = BankAccount("Hiro", createAccountNo(), 123456789, 0)
+Hiro.balance = 100
+Hiro.withdraw(50)
+Hiro.deposit(15)
+Hiro.print_receipt()
+
+# Account 3 
+Baymax = BankAccount("Baymax", createAccountNo(), 123456789, 0)
+Baymax.balance = 1000
+Baymax.withdraw(500)
+Baymax.deposit(25)
+Baymax.print_receipt()
+
+
 
 
 
