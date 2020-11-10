b181506c1ef5f3384f3aa4317de005a0c354bf60
sree-chikati <srechi453.fusdk12@gmail.com>
2020-11-09 21:28:29 -0800

    not done

--- BankAccount.py
+++ BankAccount.py
@@ -1,7 +1,16 @@
 class BankAccount:
-    #Attributes:
+    #Step 3: Attributes:
     def __init__(self, full_name, account_number, routing_number, balance):
         self.full_name = full_name
         self.account_number = account_number
         self.routing_number = routing_number
         self.balance = 0
+
+    #Step 4: Methods:
+    def deposit(self, amount):
+        """The deposit method will take one parameter amount 
+        and will add amount to the balance. Also, it will print 
+        the message: “Amount Deposited: $X.XX” """
+        self.balance += amount
+        print(f"Amount Deposited: ${self.balance}")
+        return self.balance
