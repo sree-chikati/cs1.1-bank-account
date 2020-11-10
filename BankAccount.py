113f45c76b24591af80b92df1e2ad615a405d451
sree-chikati <srechi453.fusdk12@gmail.com>
2020-11-09 22:07:19 -0800

    Step 4: Withdraw Method

--- BankAccount.py
+++ BankAccount.py
@@ -13,5 +13,24 @@ class BankAccount:
         """The deposit method will take one parameter amount 
         and will add amount to the balance. Also, it will print 
         the message: “Amount Deposited: $X.XX” """
+
         self.balance += amount
         print(f"Amount Deposited: ${amount}")
+
+    #Witdraw Method
+    def withdraw(self, amount):
+        """ The withdraw method will take one parameter amount and
+        will subtract amount from the balance. Also, it will print a 
+        message, like “Amount Withdrawn: $X.XX”. If the user tries to
+        withdraw an amount that is greater than the current balance, 
+        print ”Insufficient funds.” and charge them with an overdraft
+        fee of $10. """
+
+        if amount > self.balance:
+            print("Insufficient funds")
+            self.balance -=10
+
+        else:
+            self.balance -= amount
+            print(f"Amount Withdrawn: ${amount}")
+    
